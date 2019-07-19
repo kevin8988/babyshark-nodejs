fb4f5c3d374fe8560be090825c346d81fe6578c0
KevinChips <kevsilva07@gmail.com>
2019-07-19 15:29:57 -0300

    db url based on environment

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 const mongoose = require('mongoose');
-
 const app = require('./app');
 
-mongoose.connect(process.env.DATABASE_URL, {
+const databaseUrl = {
+  production: process.env.DATABASE_URL.replace('<PASSWORD>', process.env.DATABASE_PASSWORD).replace('<USERNAME>', process.env.DATABASE_USERNAME),
+  development: process.env.DATABASE_URL_LOCALHOST
+};
+
+mongoose.connect(databaseUrl[process.env.NODE_ENV], {
   useNewUrlParser: true
 });
 
