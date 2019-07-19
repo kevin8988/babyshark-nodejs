e6ceb10ceb0711693b26764e262036f1b394e7ba
KevinChips <kevsilva07@gmail.com>
2019-07-19 16:09:31 -0300

    user schema and model

---
+++ src/models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, 'A donate must have a first name'],
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: [true, 'A donate must have a last name'],
+    trim: true
+  },
+  email: {
+    type: String,
+    required: [true, 'A donate must have a email'],
+    trim: true
+  },
+  password: {
+    type: String,
+    required: [true, 'A donate must have a password'],
+    trim: true
+  }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
