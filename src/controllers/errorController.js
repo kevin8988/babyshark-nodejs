b18fe5a581d9720fe19b21b6ff2ffcad50c0c510
KevinChips <kevsilva07@gmail.com>
2019-07-30 15:31:40 -0300

    error handling in mode dev and mode prod

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,5 +1,17 @@
+const sendProdResponse = (res, err) => {
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
+};
+
+const sendDevResponse = (res, err) => {
+  res.status(err.statusCode).json({ status: err.status, message: err.message, error: err, stack: err.stack });
+};
+
 module.exports = (err, req, res, next) => {
-  const status = err.status || 'error';
-  const statusCode = err.statusCode || 500;
-  res.status(statusCode).json({ status, message: err.message });
+  err.status = err.status || 'error';
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'development') {
+    sendDevResponse(res, err);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendProdResponse(res, err);
+  }
 };
