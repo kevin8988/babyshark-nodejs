3a6f561f49f5d16f94eccd5243587d3e6f182a18
KevinChips <kevsilva07@gmail.com>
2019-07-30 15:17:55 -0300

    error handling for donates not found

--- src/controllers/donateController.js
+++ src/controllers/donateController.js
@@ -1,5 +1,6 @@
 const donateDAO = require('./../dao/donateDAO');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
 
 exports.getDonates = catchAsync(async (req, res, next) => {
   const { query } = req;
@@ -7,26 +8,41 @@ exports.getDonates = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: 'success', data: { donates } });
 });
 
-exports.getDonate = catchAsync(async (req, res) => {
+exports.getDonate = catchAsync(async (req, res, next) => {
   const { params } = req;
   const donate = await donateDAO.getDonate(params.id);
+
+  if (!donate) {
+    return next(new AppError(`Donate not found with this id ${params.id}`, 404));
+  }
+
   res.status(200).json({ status: 'success', data: { donate } });
 });
 
-exports.createDonate = catchAsync(async (req, res) => {
+exports.createDonate = catchAsync(async (req, res, next) => {
   const { body } = req;
   const donate = await donateDAO.create(body);
   res.status(201).json({ status: 'success', data: { donate } });
 });
 
-exports.updateDonate = catchAsync(async (req, res) => {
+exports.updateDonate = catchAsync(async (req, res, next) => {
   const { params, body } = req;
   const donate = await donateDAO.update(params.id, body);
+
+  if (!donate) {
+    return next(new AppError(`Donate not found with this id ${params.id}`, 404));
+  }
+
   res.status(200).json({ status: 'success', data: { donate } });
 });
 
-exports.deleteDonate = catchAsync(async (req, res) => {
+exports.deleteDonate = catchAsync(async (req, res, next) => {
   const { params } = req;
-  await donateDAO.delete(params.id);
+  const donate = await donateDAO.delete(params.id);
+
+  if (!donate) {
+    return next(new AppError(`Donate not found with this id ${params.id}`, 404));
+  }
+
   res.status(204).send({ status: 'success', data: null });
 });
