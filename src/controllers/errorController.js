5ca3b797f0bdfbab99149fcb6d56e9f34fcabfc5
KevinChips <kevsilva07@gmail.com>
2019-07-30 16:19:36 -0300

    validation errors

--- src/controllers/errorController.js
+++ src/controllers/errorController.js
@@ -1,3 +1,5 @@
+const AppError = require('./../utils/appError');
+
 const sendProdResponse = (res, err) => {
   res.status(err.statusCode).json({ status: err.status, message: err.message });
 };
@@ -6,12 +8,22 @@ const sendDevResponse = (res, err) => {
   res.status(err.statusCode).json({ status: err.status, message: err.message, error: err, stack: err.stack });
 };
 
+const handleValidationError = err => {
+  const errors = Object.values(err.errors)
+    .map(el => el.message)
+    .join('. ');
+
+  return new AppError(errors, 400);
+};
+
 module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
   err.statusCode = err.statusCode || 500;
   if (process.env.NODE_ENV === 'development') {
     sendDevResponse(res, err);
-  } else if (process.env.NODE_ENV === 'production') {
-    sendProdResponse(res, err);
+  } else if (process.env.NODE_ENV.trim() === 'production') {
+    let error = { ...err };
+    if (error.name === 'ValidationError') error = handleValidationError(error);
+    sendProdResponse(res, error);
   }
 };
