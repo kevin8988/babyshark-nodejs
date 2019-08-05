593e3092210f9e8ce3f4dc91da5eb82d1c2f5aef
KevinChips <kevsilva07@gmail.com>
2019-08-05 13:26:01 -0300

    create user and list all users

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,9 +1,10 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not working yet'
-  });
-};
+const catchAsync = require('./../utils/catchAsync');
+const userDAO = require('./../dao/userDAO');
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await userDAO.getAllUsers();
+  res.status(200).json({ status: 'success', results: users.length, data: { users } });
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
@@ -12,12 +13,10 @@ exports.getUser = (req, res) => {
   });
 };
 
-exports.createUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not working yet'
-  });
-};
+exports.createUser = catchAsync(async (req, res, next) => {
+  const newUser = await userDAO.createUser(req.body);
+  res.status(201).json({ status: 'success', data: { user: newUser } });
+});
 
 exports.updateUser = (req, res) => {
   res.status(500).json({
