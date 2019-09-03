581489a4eccb5924e4d447c7ff79c5402e5fa32a
KevinChips <kevsilva07@gmail.com>
2019-09-03 16:26:27 -0300

    user_address definition

--- app/models/userAddress.js
+++ app/models/userAddress.js
@@ -1,5 +1,8 @@
 module.exports = (sequelize, DataTypes) => {
-  const UserAddress = sequelize.define('UserAddress', {});
+  const UserAddress = sequelize.define('UserAddress', {
+    city: DataTypes.STRING,
+    state: DataTypes.STRING
+  });
 
   return UserAddress;
 };
