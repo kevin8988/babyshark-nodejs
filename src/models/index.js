65a409c5a7d72f7edb2806b4bd2c46a4653f5f98
KevinChips <kevsilva07@gmail.com>
2019-09-09 12:43:39 -0300

    sign up

--- src/models/index.js
+++ src/models/index.js
@@ -1,14 +1,19 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const config = require('./../../config/database');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const config = require("./../../config/database");
 
 const db = {};
 
 const sequelize = new Sequelize(config);
 
 fs.readdirSync(__dirname)
-  .filter(file => file.indexOf('.') !== 0 && file !== path.basename(__filename) && file.slice(-3) === '.js')
+  .filter(
+    file =>
+      file.indexOf(".") !== 0 &&
+      file !== path.basename(__filename) &&
+      file.slice(-3) === ".js"
+  )
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
