dd0bd6bc09779b1eb9a06df487a8606e42c157b0
KevinChips <kevsilva07@gmail.com>
2019-09-17 15:42:59 -0300

    update my event

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userController = require('../controllers/userController');
 const authController = require('../controllers/authController');
+const eventController = require('../controllers/eventController');
 
 const router = express.Router();
 
@@ -10,8 +11,13 @@ router.route('/login').post(authController.login);
 router.route('/me').get(authController.protect, userController.getMe, userController.getUser);
 router.route('/me/donates').get(authController.protect, userController.getMyDonates);
 router.route('/me/donates/interests').get(authController.protect, userController.getMyDonatesInterests);
-router.route('/me/events').get(authController.protect, userController.getMyEvents);
 router.route('/me/interests').get(authController.protect, userController.getMyInterests);
+router.route('/me/events').get(authController.protect, userController.getMyEvents);
+
+router
+  .route('/me/events/:id')
+  .patch(authController.protect, eventController.checkIfIsMyEvent, eventController.updateEvent)
+  .delete(authController.protect, eventController.checkIfIsMyEvent, eventController.deleteEvent);
 
 router.use(authController.protect, authController.restrictTo('admin'));
 
